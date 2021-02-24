0ad352abee822f0254291329326bea69afb43368
Shubham Shah <shubhamshahrising@gmail.com>
2021-02-24 23:57:36 +0530

    :sparkles: Enhanced shapes

--- src/elements/elements.test.ts
+++ src/elements/elements.test.ts
@@ -1,8 +1,8 @@
-import { Polygon } from "./index";
+import { Point, Rectangle, Triangle } from "./index";
 
-describe("Shape polygon contains method", () => {
-  test("is inside bounding box of rectangle", () => {
-    const rectangle = new Polygon([
+describe("Rectangle.contains()", () => {
+  test("is inside rectangle", () => {
+    const rectangle = new Rectangle([
       { x: 10, y: 10 },
       { x: 50, y: 10 },
       { x: 50, y: 30 },
@@ -11,8 +11,8 @@ describe("Shape polygon contains method", () => {
     expect(rectangle.contains({ x: 11, y: 11 })).toBe(true);
     expect(rectangle.contains({ x: 50, y: 30 })).toBe(true);
   });
-  test("is outside bounding box of rectangle", () => {
-    const rectangle = new Polygon([
+  test("is outside rectangle", () => {
+    const rectangle = new Rectangle([
       { x: 10, y: 10 },
       { x: 50, y: 10 },
       { x: 50, y: 30 },
@@ -20,5 +20,16 @@ describe("Shape polygon contains method", () => {
     ]);
     expect(rectangle.contains({ x: 9, y: 9 })).toBe(false);
     expect(rectangle.contains({ x: 40, y: 35 })).toBe(false);
+  });
+});
+
+describe("Triangle.contains()", () => {
+  test("is inside triangle", () => {
+    const triangle = new Triangle([
+      Point(10, 20),
+      Point(20, 20),
+      Point(15, 30)
+    ]);
+    expect(triangle.contains(Point(15, 25))).toBe(true);
   });
 });
--- src/elements/index.ts
+++ src/elements/index.ts
@@ -1,11 +1,23 @@
 export enum ShapeTypes {
-  polygon = "polygon",
-  curve = "curve"
+  rectangle = "rectangle",
+  triangle = "triangle"
+}
+
+export enum fillStyles {
+  solid = "solid",
+  zigzag = "zigzag",
+  crosshatch = "crosshatch",
+  dashed = "dashed",
+  zigzagLine = "zigzag-line"
 }
 
 type Point = {
   x: number;
   y: number;
+};
+
+export const Point = (x: number, y: number): Point => {
+  return { x, y };
 };
 
 class BoundingRect {
@@ -17,19 +29,54 @@ class BoundingRect {
   }
 }
 
-abstract class Shape {
+export interface Shape {
   type: ShapeTypes;
-  constructor(S: { type: ShapeTypes }) {
-    this.type = S.type;
-  }
-  abstract contains(P: Point): boolean;
+  fill?: {
+    style: fillStyles;
+    color: string;
+    weight: number;
+  };
+  stroke?: {
+    color: string | null;
+    width: number;
+  };
 }
-export class Polygon extends Shape {
+
+export class Fill {
+  style: fillStyles;
+  color: string;
+  weight: number;
+  constructor(style = fillStyles.solid, color = "blue", weight = 1) {
+    this.style = style;
+    this.color = color;
+    this.weight = weight;
+  }
+}
+export class Stroke {
+  color: string | null;
+  width: number;
+  constructor(color = "blue", width = 2) {
+    this.color = color;
+    this.width = width;
+  }
+}
+
+interface ExcaliShape extends Shape {
+  contains(P: Point): boolean;
+}
+
+abstract class Polygon implements ExcaliShape {
+  type: ShapeTypes;
+  fill = new Fill();
+  stroke = new Stroke();
   private _points: Array<Point>;
   private _boundingRect: BoundingRect;
 
-  constructor(points: Array<Point>) {
-    super({ type: ShapeTypes.polygon });
+  constructor(S: Shape, points: Array<Point>) {
+    const { type, fill, stroke } = S;
+    this.type = type;
+    if (fill) this.fill = fill;
+    if (stroke) this.stroke = stroke;
     this.points = points;
   }
   get points(): Array<Point> {
@@ -59,19 +106,60 @@ export class Polygon extends Shape {
     };
     this._boundingRect = new BoundingRect(min, max);
   }
-  getPoints(): Array<Point> {
-    return this._points;
+  protected _getCrossingNumber(P: Point): number {
+    let crossingNumber = 0;
+    for (let i = 0; i < this.points.length - 1; i++) {
+      const current = this.points[i];
+      const next = this.points[i + 1];
+      if (P.x <= current.x && P.x <= next.x) {
+        //downward edge
+        if (next.y > current.y) {
+          if (P.y >= current.y && P.y < next.y) {
+            crossingNumber++;
+          }
+        }
+        //upward edge
+        else if (next.y < current.y) {
+          if (P.y <= next.y && P.y > current.y) {
+            crossingNumber++;
+          }
+        }
+        //horizontal edge
+        else {
+          continue;
+        }
+      }
+    }
+    return crossingNumber;
   }
-  private _getWindingNumber(P: Point): number {
-    return 0;
-  }
-  private _isInBoundingBox(P: Point): boolean {
+  //Checks whether point P is inside of Polygon's bounding box
+  protected _isInBoundingBox(P: Point): boolean {
     const B = this._boundingRect;
     return P.x >= B.min.x && P.x <= B.max.x && P.y >= B.min.y && P.y <= B.max.y;
+  }
+  abstract contains(P: Point): boolean;
+}
+export class Rectangle extends Polygon {
+  readonly height: number;
+  readonly width: number;
+  constructor(points: Point[], config?: Shape) {
+    super({ ...config, type: ShapeTypes.rectangle }, points);
   }
   contains(P: Point): boolean {
     //Check if element is in bounding box
     return this._isInBoundingBox(P);
-    //If shape is not a rectangle then getWindingNumber
+  }
+}
+export class Triangle extends Polygon {
+  constructor(points: Point[], config?: Shape) {
+    super({ ...config, type: ShapeTypes.rectangle }, points);
+  }
+  contains(P: Point): boolean {
+    if (this._isInBoundingBox(P)) {
+      const crossingNumber = this._getCrossingNumber(P);
+      return crossingNumber % 2 !== 0;
+    } else {
+      return false;
+    }
   }
 }
